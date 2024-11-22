a8086d6e4eb9541479c79c8f9d53623882c093bb
carlososoa <carlososoa@gmail.com>
2024-11-22 15:47:43 +0000

    cambio configuracion cookie

--- controllers/logoutController.js
+++ controllers/logoutController.js
@@ -1,11 +1,15 @@
+const url_backend = process.env.URL_BACKEND
+const SameSite = process.env.SAME_SITE
+const Secure = process.env.SECURE
+
 export const logout = async (req, res) => {
   res.cookie('access_token', 'eliminado', {
-    domain: 'localhost',
+    domain: `${url_backend}`,
     path: '/',
     httpOnly: true,
     maxAge: 1000 * 60 * 60,
-    sameSite: 'lax',
-    secure: false
+    sameSite: `${SameSite}`,
+    secure: `${Secure}`
   })
     .set('Access-Control-Allow-Credentials', 'true')
     .status(200).send('Logout exitoso')
